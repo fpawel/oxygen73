7f78f4dbdf10859085c632cc386b4ce9e3528e76
binf1611@gmail.com <fpawel>
2019-10-16 21:44:23 +0300

    save:wip

--- internal/data/measurements_test.go
+++ internal/data/measurements_test.go
@@ -1,8 +1,6 @@
 package data
 
 import (
-	"github.com/jmoiron/sqlx"
-	"log"
 	"math"
 	"math/rand"
 	"testing"
@@ -12,8 +10,9 @@ import (
 func TestCreateDB(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
 	db := OpenDev()
-	newParty(db)
+	//newParty(db)
 	var mss Measurements
+	tm := time.Now()
 	for {
 		var m Measurement
 		for j := range m.Places {
@@ -22,13 +21,13 @@ func TestCreateDB(t *testing.T) {
 		m.Humidity = randV()
 		m.Pressure = randV()
 		m.Temperature = randV()
-		m.StoredAt = time.Now()
+		m.StoredAt = tm
 		mss = append(mss, m)
 
-		time.Sleep(time.Second)
+		tm = tm.Add(time.Second)
 		count := len(mss)
 
-		if count >= 10 {
+		if count >= 1000 {
 			log.Println("saving")
 			t := time.Now()
 			log.Println("save", count, time.Since(t), SaveMeasurements(mss, db))
@@ -41,22 +40,22 @@ func randV() float64 {
 	return math.Floor(rand.Float64()*100) / 100
 }
 
-func newParty(db *sqlx.DB) {
-	r, err := db.Exec(`INSERT INTO party (created_at) VALUES (?)`, time.Now())
-	if err != nil {
-		panic(err)
-	}
-	partyID, err := r.LastInsertId()
-	if err != nil {
-		panic(err)
-	}
-	for place := 0; place < 50; place++ {
-		m := int(time.Now().Month())
-		y := time.Now().Year()
-		db.MustExec(
-			`
-INSERT INTO product (party_id, serial, place  ) 
-VALUES (?, ?, ? )`,
-			partyID, place+100+y*12+m, place)
-	}
-}
+//func newParty(db *sqlx.DB) {
+//	r, err := db.Exec(`INSERT INTO party (created_at) VALUES (?)`, time.Now())
+//	if err != nil {
+//		panic(err)
+//	}
+//	partyID, err := r.LastInsertId()
+//	if err != nil {
+//		panic(err)
+//	}
+//	for place := 0; place < 50; place++ {
+//		m := int(time.Now().Month())
+//		y := time.Now().Year()
+//		db.MustExec(
+//			`
+//INSERT INTO product (party_id, serial, place  )
+//VALUES (?, ?, ? )`,
+//			partyID, place+100+y*12+m, place)
+//	}
+//}
