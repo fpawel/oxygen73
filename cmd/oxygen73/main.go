c47d999760b5f9fcb6207ec3c26415f540f7c0c7
binf1611@gmail.com <fpawel>
2019-10-16 21:46:00 +0300

    cleaned up unnecessary things

--- cmd/oxygen73/main.go
+++ cmd/oxygen73/main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"github.com/fpawel/oxygen73/internal"
 	"github.com/fpawel/oxygen73/internal/app"
 	"github.com/fpawel/oxygen73/internal/pkg"
 	"github.com/powerman/structlog"
 	"os"
-	"strconv"
 	"strings"
 )
 
@@ -19,23 +16,11 @@ func main() {
 		defaultLogLevelStr = "info"
 	}
 
-	defaultDevMode := parseBool(os.Getenv("OXYGEN73_DEVMODE"))
 	logLevel := flag.String("log.level", defaultLogLevelStr, "log `level` (debug|info|warn|err)")
-	devMode := flag.Bool("dev", defaultDevMode,
-		fmt.Sprintf("development mode on(true|false), default in OXYGEN73_DEVMODE env var: %t", defaultDevMode))
 	flag.Parse()
-	internal.DevMode = *devMode
 	pkg.InitLog()
 	structlog.DefaultLogger.SetLogLevel(structlog.ParseLevel(*logLevel))
 	app.Main()
-}
-
-func parseBool(s string) bool {
-	v, err := strconv.ParseBool(s)
-	if err != nil {
-		return false
-	}
-	return v
 }
 
 // todo: основной воркер, выполняющий опрос и запись в бд
--- internal/internal.go
+++ internal/internal.go
@@ -1,9 +1,5 @@
 package internal
 
-var (
-	DevMode bool // флаг режима разработки
-)
-
 const (
 	WindowClass       = "Oxygen73WindowClass"
 	DelphiWindowClass = "TFormOxygen73"
