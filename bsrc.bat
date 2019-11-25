2d5faa7366829d0e882ff8913ed0f31661f4fd1d
binf1611@gmail.com <fpawel>
2019-11-25 16:29:42 +0300

    script: save sources

---
+++ bsrc.bat
@@ -0,0 +1,3 @@
+git clone https://github.com/fpawel/oxygen73 build\oxygen73src\oxygen73
+git clone https://github.com/fpawel/oxygen73gui build\oxygen73src\oxygen73gui
+7z a -tzip -sdel build\oxygen73src.zip ".\build\oxygen73src"
